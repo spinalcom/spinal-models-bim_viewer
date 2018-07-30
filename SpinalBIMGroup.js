87a091a5b2352401ef3c6b76dc5839069c72dcc9
hossam-basudan <the.hosam@yahoo.com>
2018-07-30 17:13:19 +0200

    only in fileSystem

--- SpinalBIMGroup.js
+++ SpinalBIMGroup.js
@@ -1,18 +1,19 @@
 const spinalCore = require("spinal-core-connectorjs");
 const globalType = typeof window === "undefined" ? global : window;
 
-
 class SpinalBIMGroup extends globalType.Model {
   constructor(_name, _BIMObjects, _display, _color, name = "SpinalBIMGroup") {
     super();
-    this.add_attr({
-      name: _name || "",
-      BIMObjects: _BIMObjects || [],
-      display: _display || true,
-      color: _color || ""
-    });
+    if (FileSystem._sig_server) {
+      this.add_attr({
+        name: _name || "",
+        BIMObjects: _BIMObjects || [],
+        display: _display || true,
+        color: _color || ""
+      });
+    }
   }
 }
 
 module.exports = SpinalBIMGroup;
-spinalCore.register_models([SpinalBIMGroup])
+spinalCore.register_models([SpinalBIMGroup]);
